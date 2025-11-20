2a129483dbab0616c660fa0503c1580bbb6dea23
Ernst Junior <e6306412@cmaisonneuve.qc.ca>
2025-11-20 10:49:09 -0500

    j'ai reset AuthController et Auth ⚠️NE PAS TOUCHER AU BACKEND POUR L'INSTANT

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,16 +1,7 @@
 import express from "express";
-import { login, register } from "../controllers/authController.js";
 
 
 const router = express.Router();
 
-// route inscription
-router.post("/register", register);
-
-// route connexion
-router.post("/login", login);
-
-// route déconnexion
-router.post("/logout", logout);
 
 export default router;
